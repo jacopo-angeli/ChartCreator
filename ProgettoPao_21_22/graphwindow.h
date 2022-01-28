d2340b72da22837784b9ec69c6e521e7dcc86332
jacopo-angeli <diobellissimo100@gmail.com>
2022-01-28 11:57:02 +0100

    Initial import

---
+++ ProgettoPao_21_22/graphwindow.h
@@ -0,0 +1,60 @@
+#ifndef GRAPHWINDOW_H
+#define GRAPHWINDOW_H
+
+#include "mainwindow.h"
+
+#include <QWidget>
+#include <QWidgetAction>
+#include <QStringList>
+#include <QColorDialog>
+#include <QtCharts>
+#include <QWindow>
+#include <QMenuBar>
+#include <QMenu>
+#include <QToolBar>
+#include <QToolButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QFrame>
+
+class GraphWindow : public QWidget
+{
+    Q_OBJECT
+private:
+    QTableWidget *_ValueTab;
+    QWidget *_ChartWindow;
+    QChart *_Chart;
+    QToolBar *_UpperToolBar;
+    QChartView *_ChartView;
+
+    std::pair<int, int> getValueRange() const;
+
+    void showChartWindow();
+
+    void LineChart(QString ="Default Title");
+    void LineToolBar();
+
+    void BarChart();
+
+    void BarToolBar();
+
+    void PieToolBar();
+
+    void PieChart();
+
+public:
+    GraphWindow(QTableWidget *, QWidget * = nullptr);
+public slots:
+    void BarChartCreator();
+    void PieChartCreator();
+
+    void LineChartCreator();
+
+    void SaveAsPng();
+
+    void ChartSelection();
+
+    void explodeSlice();
+};
+
+#endif // GRAPHWINDOW_H
