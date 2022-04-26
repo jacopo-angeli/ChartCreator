bd6dac7a6523e18db9579b5025e3a6d194d2f54b
jacopo-angeli <diobellissimo100@gmail.com>
2022-04-26 12:02:12 +0200

    Works done in controller.h and .cpp

--- controller.cpp
+++ controller.cpp
@@ -1,3 +1,44 @@
 #include "controller.h"
 
-Controller::Controller() {};
+Controller::Controller(): _MainWindow(new MainWindow), _ChartWindows(QVector<ChartWindow*>()), _ChartSelection(), _ChartSettings{}
+
+
+void Controller::NewChart(){
+
+}
+void Controller::ChangeChart(){
+
+}
+void Controller::UpperInsert(){
+
+}
+void Controller::LowerInsert(){
+
+}
+void Controller::LeftInsert(){
+
+}
+void Controller::RightInsert(){
+
+}
+void Controller::TableReset(){
+
+};
+void Controller::RowReset(){
+
+};
+void Controller::ColumnReset(){
+
+};
+void Controller::LeftAlign(){
+
+};
+void Controller::CenterAlign(){
+
+};
+void Controller::RightAlign(){
+
+};
+void Controller::SpinBoxRefresh(){
+
+};
--- controller.h
+++ controller.h
@@ -3,6 +3,8 @@
 
 #include "mainwindow.h"
 #include "chartwindow.h"
+#include "chartselection.h"
+#inlcude "chartsettings.h"
 
 #include <QObject>
 #include <QVector>
@@ -14,26 +16,28 @@ class Controller : public QWidget
 private:
     MainWindow* _MainWindow;
     QVector<ChartWindow*> _ChartWindows;
+    ChartSelection* _ChartSelection;
+    ChartSettings* _ChartSettings;
 public:
     Controller();
 public slots:
-//    void NewChart(); //TBD : void o bool? Parametri?
-//    void ChangeChart(); //TBD : void o bool? Parametri?
+    void NewChart(); //TBD : void o bool? Parametri?
+    void ChangeChart(); //TBD : void o bool? Parametri?
 
     /* MainWindow slots */
-//    void UpperInsert();
-//    void LowerInsert();
-//    void LeftInsert();
-//    void RightInsert();
-//    void TableReset();
-//    void RowReset();
-//    void ColumnReset();
-//    void RowDelete();
-//    void ColumnDelete();
-//    void LeftAlign();
-//    void CenterAlign();
-//    void RightAlign();
-//    void SpinBoxRefresh();
+    void UpperInsert();
+    void LowerInsert();
+    void LeftInsert();
+    void RightInsert();
+    void TableReset();
+    void RowReset();
+    void ColumnReset();
+    void RowDelete();
+    void ColumnDelete();
+    void LeftAlign();
+    void CenterAlign();
+    void RightAlign();
+    void SpinBoxRefresh();
 };
 
 #endif // CONTROLLER_H
