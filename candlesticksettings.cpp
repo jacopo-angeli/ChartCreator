ac59bbe3e5508d9f2b8336f25728a1a9d64a2621
UraganoAngelina <alberto.angeloni@studenti.unipd.it>
2022-05-11 16:23:04 +0200

    settings ospedale

--- candlesticksettings.cpp
+++ candlesticksettings.cpp
@@ -11,62 +11,97 @@ CandleStickSettings::CandleStickSettings(QWidget* parent) : ChartSettings(new Ca
     QHBoxLayout * hor5=new QHBoxLayout();
     QHBoxLayout * hor6=new QHBoxLayout();
 
-    QVBoxLayout * _imp=new QVBoxLayout(); //layout per contenere la chartview
-    QVBoxLayout * _main=new QVBoxLayout(); //layout per contenere vista e settings
-
-
-
     //buttons
 
-    QPushButton* _btn1=new QPushButton("PICK");
-    _btn1->setMaximumHeight(50);
-    _btn1->setMaximumWidth(250);
+    QPushButton* btn1=new QPushButton("PICK");
+    btn1->setMaximumHeight(50);
+    btn1->setMaximumWidth(250);
 
-    QPushButton* _btn2=new QPushButton("PICK");
-    _btn2->setMaximumHeight(50);
-    _btn2->setMaximumWidth(250);
+    QPushButton* btn2=new QPushButton("PICK");
+    btn2->setMaximumHeight(50);
+    btn2->setMaximumWidth(250);
 
-    QPushButton* _btn3=new QPushButton("PICK");
-    _btn3->setMaximumHeight(50);
-    _btn3->setMaximumWidth(250);
+    QPushButton* btn3=new QPushButton("PICK");
+    btn3->setMaximumHeight(50);
+    btn3->setMaximumWidth(250);
 
-    QPushButton* _btn4=new QPushButton("PICK");
-    _btn4->setMaximumHeight(50);
-    _btn4->setMaximumWidth(250);
+    QPushButton* btn4=new QPushButton("PICK");
+    btn4->setMaximumHeight(50);
+    btn4->setMaximumWidth(250);
 
-    QPushButton* _btn5=new QPushButton("PICK");
-    _btn5->setMaximumHeight(50);
-    _btn5->setMaximumWidth(250);
+    QPushButton* btn5=new QPushButton("PICK");
+    btn5->setMaximumHeight(50);
+    btn5->setMaximumWidth(250);
 
-    QPushButton* _btn6=new QPushButton("PICK");
-    _btn6->setMaximumHeight(50);
-    _btn6->setMaximumWidth(250);
+    QPushButton* btn6=new QPushButton("PICK");
+    btn6->setMaximumHeight(50);
+    btn6->setMaximumWidth(250);
 
     //text labels
 
-    QLabel* _txt1=new QLabel("text");
-    _txt1->setMaximumHeight(50);
-    _txt1->setMaximumWidth(250);
+    QLabel* txt1=new QLabel("text");
+    txt1->setMaximumHeight(50);
+    txt1->setMaximumWidth(250);
 
-    QLabel* _txt2=new QLabel("text");
-    _txt2->setMaximumHeight(50);
-    _txt2->setMaximumWidth(250);
+    QLabel* txt2=new QLabel("text");
+    txt2->setMaximumHeight(50);
+    txt2->setMaximumWidth(250);
 
-    QLabel* _txt3=new QLabel("text");
-    _txt3->setMaximumHeight(50);
-    _txt3->setMaximumWidth(250);
+    QLabel* txt3=new QLabel("text");
+    txt3->setMaximumHeight(50);
+    txt3->setMaximumWidth(250);
 
-    QLabel* _txt4=new QLabel("text");
-    _txt4->setMaximumHeight(50);
-    _txt4->setMaximumWidth(250);
+    QLabel* txt4=new QLabel("text");
+    txt4->setMaximumHeight(50);
+    txt4->setMaximumWidth(250);
 
-    QLabel* _txt5=new QLabel("text");
-    _txt5->setMaximumHeight(50);
-    _txt5->setMaximumWidth(250);
+    QLabel* txt5=new QLabel("text");
+    txt5->setMaximumHeight(50);
+    txt5->setMaximumWidth(250);
 
 
-    QLabel* _txt6=new QLabel("text");
-    _txt6->setMaximumHeight(50);
-    _txt6->setMaximumWidth(250);
+    QLabel* txt6=new QLabel("text");
+    txt6->setMaximumHeight(50);
+    txt6->setMaximumWidth(250);
+
+    /*
+    QLabel* _OpeningPrices;
+    QLabel* _ClosingPrices;
+    QLabel* _BottomPrices;
+    QLabel* _LowestPrices;
+    QLabel* _HighestPrices;
+    QLabel* _Categories;
+    */
+    //setting layout
+    hor1->addWidget(_OpeningPrices);
+    hor1->addWidget(btn1);
+    hor1->addWidget(txt1);
+
+    hor2->addWidget(_ClosingPrices);
+    hor2->addWidget(btn2);
+    hor2->addWidget(txt2);
+
+    hor3->addWidget(_BottomPrices);
+    hor3->addWidget(btn3);
+    hor3->addWidget(txt3);
+
+    hor4->addWidget(_LowestPrices);
+    hor4->addWidget(btn4);
+    hor4->addWidget(txt4);
+
+    hor5->addWidget(_HighestPrices);
+    hor5->addWidget(btn5);
+    hor5->addWidget(txt5);
+
+    hor6->addWidget(_Categories);
+    hor6->addWidget(btn6);
+    hor6->addWidget(txt6);
+
+    _Settings->setLayout(hor1);
+    _Settings->setLayout(hor2);
+    _Settings->setLayout(hor3);
+    _Settings->setLayout(hor4);
+    _Settings->setLayout(hor5);
+    _Settings->setLayout(hor6);
 
 }
--- chartsettings.cpp
+++ chartsettings.cpp
@@ -16,7 +16,39 @@
 #include <QFrame>
 ChartSettings::ChartSettings(Chart * chart,QWidget * parent) : QWidget(parent),  _Settings(new QGroupBox(this)), _ChartView(new QChartView(this)), _Chart(chart), _Title(new QLabel("Unset")), _Color(new QComboBox())
 {
+    _ChartView->setChart(_Chart);
+    QHBoxLayout * hor1=new QHBoxLayout();
+    QHBoxLayout * hor2=new QHBoxLayout();
+    QVBoxLayout * main=new QVBoxLayout();
 
+    main->addWidget(_ChartView);
+
+    QPushButton* btn1=new QPushButton("PICK");
+    btn1->setMaximumHeight(50);
+    btn1->setMaximumWidth(250);
+
+    QPushButton* btn2=new QPushButton("PICK");
+    btn2->setMaximumHeight(50);
+    btn2->setMaximumWidth(250);
+
+    QLabel* txt1=new QLabel("text");
+    txt1->setMaximumHeight(50);
+    txt1->setMaximumWidth(250);
+
+    QLabel* txt2=new QLabel("text");
+    txt2->setMaximumHeight(50);
+    txt2->setMaximumWidth(250);
+
+    //horizontal
+    hor1->addWidget(_Title);
+    hor1->addWidget(btn1);
+    hor1->addWidget(txt1);
+    hor2->addWidget(_Color);
+    hor2->addWidget(btn2);
+    hor2->addWidget(txt2);
+    _Settings->setLayout(hor1);
+    _Settings->setLayout(hor2);
+    main->addWidget(_Settings);
 }
 
 
